1f02e6020c13cbfe263f1ff81f5c8a1f6fcddcde
Kashish <kashish.biserwal@protonmail.com>
2023-01-13 14:35:11 +0530

    cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const app = express();
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const errorMiddleware = require("./middleware/error")
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
@@ -13,6 +15,10 @@ const user = require("./routes/userRoute")
 
 app.use("/api", product);
 app.use("/api", user);
+
+app.get("/", (req, res) => {
+    res.send("Figurz api on vercel.");
+});
 
 //middleware for errors
 app.use(errorMiddleware)
