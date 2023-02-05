1ac57599a753fa2aac8351dc8f62a21b5d007e74
Kashish <kashish.biserwal@protonmail.com>
2023-02-06 00:51:23 +0530

    bp test

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const cors = require('cors');
@@ -9,6 +10,9 @@ const errorMiddleware = require("./middleware/error")
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute")
